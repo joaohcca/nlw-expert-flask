86c5a434943270b48e2f333b64db08bf93d8e019
Joao Henrique Albuquerque <joaohccalbu@gmail.com>
2024-02-11 12:08:22 -0300

    feat: implementing validator to app

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,8 +1,20 @@
 from src.views.https_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        #enviar para servico de log
+        #disparar envio de email
+        return HttpResponse(status_code=error.status_code, body={
+            "errors": [{
+                "title": error.name,
+                "detail": error.message
+            }]
+        }
+    )
+
     return HttpResponse(
-        status_code=500,
+        status_code=422,
         body={
             "errors": [{
             "title": "Server Error",
